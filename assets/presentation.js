efdd388e66c304781a94fdeb923304e14792b224
Jean-françois LEPANTE <jean-francois@homypay.com>
2021-11-12 20:37:16 +0100

    scrollspy in place

--- assets/presentation.js
+++ assets/presentation.js
@@ -1,20 +1,18 @@
 import './styles/presentation.scss';
-import { card, Toast, Popover } from 'bootstrap';
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "@fortawesome/fontawesome-free/js/all.js";
-// import './scripts/intro.js';
-// import './bootstrap';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
-
-
-// start the Stimulus application
-
+// import { createPopper } from '@popperjs/core';
 import $ from 'jquery';
 global.$ = $;
+// import 'bootstrap';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+// import './scripts/dropdown';
+// import './scripts/intro.js';
+// import './bootstrap';
+// any CSS you import will output into a single css file (app.css in this case)
+
+// start the Stimulus application
+console.log('test');
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#list-example'
+})
+
